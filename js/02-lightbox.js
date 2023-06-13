4ea80d02687a1ca5c970be3c99312ec1f952bcae
AntonTy35 <antony.tislenko@gmail.com>
2023-06-13 10:23:17 +0300

    task-02

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,46 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
 console.log(galleryItems);
+
+const galleryEls = document.querySelector(".gallery");
+
+const galleryItemsEl = galleryItems
+  .map(
+    (galleryItems) =>
+      `<li class="gallery__item">
+      <a class="gallery__link" href="${galleryItems.original}">
+        <img
+          class="gallery__image"
+          src="${galleryItems.preview}"
+          alt="${galleryItems.description}"
+          />
+          </a>
+          </li>`
+  )
+  .join("");
+
+galleryEls.insertAdjacentHTML("beforeend", galleryItemsEl);
+
+galleryEls.addEventListener("click", onClick);
+
+function onClick(event) {
+  event.preventDefault();
+
+  if (event.target.nodeName !== "IMG") {
+    return;
+  }
+
+  galleryEls.addEventListener("keydown", onPressEscape);
+
+  function onPressEscape(evt) {
+    if (evt.key === "Escape") {
+      imageWindow.close();
+    }
+  }
+}
+
+new SimpleLightbox(".gallery a", {
+  captionDelay: 250,
+  captionsData: "alt",
+});
